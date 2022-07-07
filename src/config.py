cb2f6ef0c6b1c4701456066a0c349f6d0c8b7c02
XiangyuSu611 <15524218980@163.com>
2022-07-07 12:41:48 +0000

    big update

---
+++ src/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+"""
+Directory configuration.
+"""
+LOCAL_ROOT = Path('/home/code/TMT')
+DATA_ROOT = Path(LOCAL_ROOT, 'data')
+
+
---
+++ src/data_generation/preprocess/exemplars/crop_and_center copy.py
@@ -0,0 +1,62 @@
+"""
+Crop and center initial photographs.
+"""
+
+import config
+import numpy as np
+
+from pathlib import Path
+from skimage.io import imread, imsave
+from skimage.color import rgb2gray, gray2rgb
+from skimage.transform import resize
+from thirdparty.toolbox.masks import mask_to_bbox, crop_bbox
+
+
+def bright_pixel_mask(image, percentile=80):
+    # get forward mask.
+    image = rgb2gray(image)
+    perc = np.percentile(np.unique(image), percentile)
+    mask = image < perc
+    return mask
+
+
+def bright_pixel_bbox(image, percentile=80):
+    # change mask to bounding box.
+    bbox = mask_to_bbox(bright_pixel_mask(image, percentile))
+    return bbox
+
+
+def make_square_image(image, max_size):
+    # re-save corpped image.
+    fg_bbox = bright_pixel_bbox(image, percentile=80)
+    cropped_im = crop_bbox(image, fg_bbox)
+    output_im = np.full((max_size, max_size, 3), dtype=cropped_im.dtype,
+                        fill_value=255)
+    height, width = cropped_im.shape[:2]
+    if height >= width:
+        new_width = int(width * max_size / height)
+        padding = (max_size - new_width) // 2
+        cropped_im = resize(cropped_im, (max_size, new_width),
+                            mode='constant', cval=255.0, anti_aliasing=True) * 255
+        output_im[:, padding:padding + new_width] = cropped_im[:, :, :3]
+    else:
+        new_height = int(height * max_size / width)
+        padding = (max_size - new_height) // 2
+        cropped_im = resize(cropped_im, (new_height, max_size),
+                            mode='constant', cval=255.0, anti_aliasing=True) * 255
+        output_im[padding:padding + new_height, :] = cropped_im[:, :, :3]
+    return output_im
+
+
+def main(directory):
+    directory = Path(directory).resolve()
+    for path in sorted(directory.iterdir()):
+        image = imread(str(path))
+        if len(image.shape) == 2:
+            image = gray2rgb(img)
+        cropped_image = make_square_image(image, max_size=1000)
+        imsave(path.replace('original', 'cropped'), cropped_image)
+
+
+if __name__ == '__main__':
+    main()
